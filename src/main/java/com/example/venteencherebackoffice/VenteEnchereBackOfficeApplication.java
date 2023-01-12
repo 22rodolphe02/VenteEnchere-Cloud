0f22ac42f69fe20d27568e1bfca53695cdf032f2
22rodolphe02 <tsiafararodolphe@gmail.com>
2023-01-13 01:00:49 +0300

    initialisation

---
+++ src/main/java/com/example/venteencherebackoffice/VenteEnchereBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.venteencherebackoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenteEnchereBackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenteEnchereBackOfficeApplication.class, args);
+	}
+
+}
