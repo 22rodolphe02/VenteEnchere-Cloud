eb5b61b1c20d49433248f728d0aba8eca6c9668b
22rodolphe02 <98727714+22rodolphe02@users.noreply.github.com>
2023-01-20 01:58:04 +0300

    Update SoldeRepository.java

--- src/main/java/com/enchere/postgres/repos/SoldeRepository.java
+++ src/main/java/com/enchere/postgres/repos/SoldeRepository.java
@@ -2,7 +2,6 @@ package com.enchere.postgres.repos;
 
 import com.enchere.postgres.models.Solde;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,11 +9,13 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface SoldeRepository extends JpaRepository<Solde, Integer> {
+    List<Solde> findByValiderAndMouvementSolde_Id(Boolean valider, Integer id);
+
     @Transactional
     @Modifying
     @Query("update Solde s set s.valider = ?1 where s.id = ?2")
     int updateValiderById(Boolean valider, Integer id);
 
 
-    List<Solde> findAllByValider(boolean isValid);
+    List<Solde> findByValider(boolean b);
 }
