80125ae2692312c9e4baaa6cf1aa07da33fb26d5
22rodolphe02 <tsiafararodolphe@gmail.com>
2023-02-10 17:11:13 +0300

    Update Database.java

--- src/main/java/com/enchere/utils/Database.java
+++ src/main/java/com/enchere/utils/Database.java
@@ -9,12 +9,12 @@ import java.sql.SQLException;
 public class Database {
 
     private static Connection connection;
-    private static final String host = "containers-us-west-102.railway.app";
+    private static final String host = "containers-us-west-19.railway.app";
 
     private static final String database = "railway";
-    private static final int port = 6347;
+    private static final int port = 6336;
     private static final String user = "postgres";
-    private static final String pass = "muWcYv3UjOvmQyqrcSWu";
+    private static final String pass = "jYeEtXwfTysoGJBtzHDg";
     private static String url = "jdbc:postgresql://%s:%d/%s";
 
     public Database()
